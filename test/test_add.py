9a2c69282b888586981af7f3dc9215ddc493421e
Fernando Perez <fernando.perez.lara@outlook.com>
2023-02-28 12:45:03 +0100

    test: create test

---
+++ test/test_add.py
@@ -0,0 +1,24 @@
+"""This module test the add command."""
+from faker import Faker
+import random
+import unittest
+
+from scripts.commands.add.task import Add
+from scripts.utils.constants import API_URL
+from scripts.utils.request import Request
+
+
+class TestAdd(unittest.TestCase):
+    """Test the add command."""
+    def test_add(self):
+        """Test the add command."""
+        fake = Faker('es_ES')
+        names = [fake.name() for i in range(random.randint(5, 10))]
+
+        for name in names:
+            Add(name).run()
+
+        list_items = Request(API_URL, timeout=2.5).get()
+
+        for name in names:
+            self.assertIn(name, [item['text'] for item in list_items])
