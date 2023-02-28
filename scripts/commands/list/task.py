5088de786a7a453c44c118c823bcbf5dff8021f8
Fernando Perez <fernando.perez.lara@outlook.com>
2023-02-28 10:57:43 +0100

    feat: display a list of todo

--- scripts/commands/list/task.py
+++ scripts/commands/list/task.py
@@ -1,6 +1,9 @@
 """This module implements the tasks related to display a list of todo."""
+import json
+
 from scripts.utils.constants import API_URL
 from scripts.utils.request import Request
+
 
 class List():
     """Task to list all todo items."""
@@ -10,4 +13,28 @@ class List():
     def run(self):
         """Run the task."""
         request = Request(API_URL, timeout=2.5).get()
-        print(f"Output: {request}")
+
+        if self._output == 'table':
+            if len(request) == 0:
+                col_widths = [0, 0]
+            else:
+                col_widths = [
+                    max(len(item['_id']) for item in request),
+                    max(len(item['text']) for item in request)
+                ]
+
+            print(
+                "ID".ljust(col_widths[0]) + ' ' +
+                "ITEM".ljust(col_widths[1])
+            )
+
+            for item in request:
+                print(
+                    item['_id'].ljust(col_widths[0]) + ' ' +
+                    item['text'].ljust(col_widths[1])
+                )
+        elif self._output == 'txt':
+            for item in request:
+                print(item['text'])
+        else:
+            print(json.dumps(request, indent=2, sort_keys=True))
